f83fcc1215d758049aac8796498cd57a5e81fc2c
Fadl Lul Hakim Ihsan <48132237+HakimIhsan@users.noreply.github.com>
2023-11-22 09:59:59 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 const form = document.getElementById("chat-form");
 const input = document.getElementById("chat-input");
 const messages = document.getElementById("chat-messages");
-const apiKey = "";
+const apiKey = "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";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const message = input.value;
+  const userMessage = input.value;
   input.value = "";
 
   messages.innerHTML += `<div class="message user-message">
-  <img src="./icons/user.png" alt="user icon"> <span>${message}</span>
+    <img src="./icons/user.png" alt="user icon"> <span>${userMessage}</span>
   </div>`;
+
+  // Extract information from the table for the chatbot prompt
+  const tableRows = document.querySelectorAll("#table tr");
+  const rowData = {};
+
+  tableRows.forEach((row, rowIndex) => {
+    const columns = row.querySelectorAll("td");
+
+    columns.forEach((column, columnIndex) => {
+      // Assuming the first row contains column headers and the first column contains row titles
+      if (rowIndex === 0 && columnIndex !== 0) {
+        const columnHeader = column.textContent.trim();
+        rowData[columnHeader] = {};
+      } else if (rowIndex !== 0 && columnIndex === 0) {
+        const rowTitle = column.textContent.trim();
+        rowData[rowTitle] = {};
+      } else if (rowIndex !== 0 && columnIndex !== 0) {
+        const columnHeader = tableRows[0].querySelectorAll("td")[columnIndex].textContent.trim();
+        const rowTitle = tableRows[rowIndex].querySelectorAll("td")[0].textContent.trim();
+        rowData[rowTitle][columnHeader] = column.textContent.trim();
+      }
+    });
+  });
+
+  // Use the table data as part of the prompt
+  const prompt = `${userMessage} ${JSON.stringify(rowData)}`;
 
   // Use axios library to make a POST request to the OpenAI API
   const response = await axios.post(
     "https://api.openai.com/v1/completions",
     {
-      prompt: message,
+      prompt,
       model: "text-davinci-003",
       temperature: 0,
       max_tokens: 1000,
@@ -31,9 +57,10 @@ form.addEventListener("submit", async (e) => {
       },
     }
   );
+
   const chatbotResponse = response.data.choices[0].text;
 
   messages.innerHTML += `<div class="message bot-message">
-  <img src="./icons/chatbot.png" alt="bot icon"> <span>${chatbotResponse}</span>
+    <img src="./icons/chatbot.png" alt="bot icon"> <span>${chatbotResponse}</span>
   </div>`;
 });
